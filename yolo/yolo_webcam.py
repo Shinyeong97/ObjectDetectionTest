23d9ad6317f46267d8a21f6a78c7112f9717db67
Shinyeong97 <shinyeong.yun@gmail.com>
2020-01-22 16:07:17 -0500

    Update other weights & cfg files

--- yolo/yolo_webcam.py
+++ yolo/yolo_webcam.py
@@ -1,4 +1,6 @@
+#-----------------------------------------------------------
 # import the necessary packages
+#-----------------------------------------------------------
 import numpy as np
 import argparse
 import imutils
@@ -6,51 +8,67 @@ import time
 import cv2
 import os
  
+#-----------------------------------------------------------
 # construct the argument parse and parse the arguments
+#-----------------------------------------------------------
 ap = argparse.ArgumentParser()
 
 ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO directory")
-ap.add_argument("-d", "--device", type=int, default=0, help="device number using for the detection(usually 0 for front cam and 1 for rear cam")
+ap.add_argument("-d", "--device", type=int, default=0, help="device number for the detection(usually 0 for front cam and 1 for rear cam")
 ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
 
+#-----------------------------------------------------------
 # load the COCO class labels our YOLO model was trained on
+#-----------------------------------------------------------
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
  
     
+#-----------------------------------------------------------
 # initialize a list of colors to represent each possible class label
+#-----------------------------------------------------------
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
  
     
+#-----------------------------------------------------------
 # derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
-configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+#-----------------------------------------------------------
+weightsPath = os.path.sep.join([args["yolo"], "yolov3-tiny.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3-tiny.cfg"])
  
     
+#-----------------------------------------------------------
 # load our YOLO object detector trained on COCO dataset (80 classes)
 # and determine only the *output* layer names that we need from YOLO
+#-----------------------------------------------------------
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
+#-----------------------------------------------------------
 # initialize the video stream, pointer to output video file, and
 # frame dimensions
+#-----------------------------------------------------------
 cap = cv2.VideoCapture(args["device"])
 writer = None
 (W, H) = (None, None)
 
 
 print("[INFO] press 'q' button to close the window...")
+print("[INFO] device number: ", args["device"])
+#-----------------------------------------------------------
 # loop over frames from the video file stream
+#-----------------------------------------------------------
 while True:
     # read the next frame from the file
     ret, frame = cap.read()
+    
     
     # if the frame was not grabbed, then we have reached the end
     # of the stream]    
@@ -61,13 +79,13 @@ while True:
     # when you use a front cam, flip it horizontally
     if args["device"] == 0:
         frame = cv2.flip(frame, 1)
- 
+
     # if the frame dimensions are empty, grab them
     if W is None or H is None:
         (H, W) = frame.shape[:2]
 
 
-# construct a blob from the input frame and then perform a forward
+    # construct a blob from the input frame and then perform a forward
     # pass of the YOLO object detector, giving us our bounding boxes
     # and associated probabilities
     blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
@@ -75,6 +93,7 @@ while True:
     start = time.time()
     layerOutputs = net.forward(ln)
     end = time.time()
+    #print("[1]", end-start)
  
     # initialize our lists of detected bounding boxes, confidences,
     # and class IDs, respectively
@@ -82,7 +101,7 @@ while True:
     confidences = []
     classIDs = []
     
-
+    start2 = time.time()
     # loop over each of the layer outputs
     for output in layerOutputs:
         # loop over each of the detections
@@ -125,6 +144,7 @@ while True:
         var_b = 0
         # loop over the indexes we are keeping
         for i in idxs.flatten():
+            print("...")
             var_b = 0
             # extract the bounding box coordinates
             (x, y) = (boxes[i][0], boxes[i][1])
@@ -135,12 +155,21 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
             text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
             cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                var_b = 1
-                break
-        if var_b == 1:
-            break
+#             cv2.imshow('Video', frame)
+    
+#             if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 var_b = 1
+#                 break
+#         if var_b == 1:
+#             break
+
+    cv2.imshow('Video', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+            
+    end2 = time.time()
+    #print("[2]", end2-start2, "\n")
 
 
 # release the file pointers
